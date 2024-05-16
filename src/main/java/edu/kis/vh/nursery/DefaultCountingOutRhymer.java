400a0818c3afedd65ef4dfecb9e5b37603bcb074
Pawel Hase <pawelhase@gmail.com>
2024-05-16 15:04:54 +0200

    zad.17

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -21,6 +21,9 @@ public class DefaultCountingOutRhymer {
             numbers[++total] = in;
     }
 
+    /**
+     * @return czy numbers jest puste
+     */
     public boolean callCheck() {
         return total == wartoscBledu;
     }
